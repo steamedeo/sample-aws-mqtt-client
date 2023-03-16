4c0854920ca44b1438f61de87efb6d0201d7001d
Stefano <ste.amedeo@gmail.com>
2023-03-16 11:24:14 +0100

    Added windows ps script

--- pubsub.py
+++ pubsub.py
@@ -1,12 +1,14 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0.
 
+import command_line_utils
 from awscrt import mqtt
 import sys
 import threading
 import time
 from uuid import uuid4
 import json
+import random2
 
 # This sample uses the Message Broker for AWS IoT to send and receive messages
 # through an MQTT connection. On startup, the device connects to the server,
@@ -15,18 +17,24 @@ import json
 # since it is subscribed to that same topic.
 
 # Parse arguments
-import command_line_utils;
-cmdUtils = command_line_utils.CommandLineUtils("PubSub - Send and recieve messages through an MQTT connection.")
+cmdUtils = command_line_utils.CommandLineUtils(
+    "PubSub - Send and recieve messages through an MQTT connection.")
 cmdUtils.add_common_mqtt_commands()
 cmdUtils.add_common_topic_message_commands()
 cmdUtils.add_common_proxy_commands()
 cmdUtils.add_common_logging_commands()
-cmdUtils.register_command("key", "<path>", "Path to your key in PEM format.", True, str)
-cmdUtils.register_command("cert", "<path>", "Path to your client certificate in PEM format.", True, str)
-cmdUtils.register_command("port", "<int>", "Connection port. AWS IoT supports 443 and 8883 (optional, default=auto).", type=int)
-cmdUtils.register_command("client_id", "<str>", "Client ID to use for MQTT connection (optional, default='test-*').", default="test-" + str(uuid4()))
-cmdUtils.register_command("count", "<int>", "The number of messages to send (optional, default='10').", default=10, type=int)
-cmdUtils.register_command("is_ci", "<str>", "If present the sample will run in CI mode (optional, default='None')")
+cmdUtils.register_command(
+    "key", "<path>", "Path to your key in PEM format.", True, str)
+cmdUtils.register_command(
+    "cert", "<path>", "Path to your client certificate in PEM format.", True, str)
+cmdUtils.register_command(
+    "port", "<int>", "Connection port. AWS IoT supports 443 and 8883 (optional, default=auto).", type=int)
+cmdUtils.register_command(
+    "client_id", "<str>", "Client ID to use for MQTT connection (optional, default='test-*').", default="test-" + str(uuid4()))
+cmdUtils.register_command(
+    "count", "<int>", "The number of messages to send (optional, default='10').", default=10, type=int)
+cmdUtils.register_command(
+    "is_ci", "<str>", "If present the sample will run in CI mode (optional, default='None')")
 # Needs to be called so the command utils parse the commands
 cmdUtils.get_args()
 
@@ -35,13 +43,16 @@ received_all_event = threading.Event()
 is_ci = cmdUtils.get_command("is_ci", None) != None
 
 # Callback when connection is accidentally lost.
+
+
 def on_connection_interrupted(connection, error, **kwargs):
     print("Connection interrupted. error: {}".format(error))
 
 
 # Callback when an interrupted connection is re-established.
 def on_connection_resumed(connection, return_code, session_present, **kwargs):
-    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+    print("Connection resumed. return_code: {} session_present: {}".format(
+        return_code, session_present))
 
     if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
         print("Session did not persist. Resubscribing to existing topics...")
@@ -53,12 +64,12 @@ def on_connection_resumed(connection, return_code, session_present, **kwargs):
 
 
 def on_resubscribe_complete(resubscribe_future):
-        resubscribe_results = resubscribe_future.result()
-        print("Resubscribe results: {}".format(resubscribe_results))
+    resubscribe_results = resubscribe_future.result()
+    print("Resubscribe results: {}".format(resubscribe_results))
 
-        for topic, qos in resubscribe_results['topics']:
-            if qos is None:
-                sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+    for topic, qos in resubscribe_results['topics']:
+        if qos is None:
+            sys.exit("Server rejected resubscribe to topic: {}".format(topic))
 
 
 # Callback when the subscribed topic receives a message
@@ -69,8 +80,10 @@ def on_message_received(topic, payload, dup, qos, retain, **kwargs):
     if received_count == cmdUtils.get_command("count"):
         received_all_event.set()
 
+
 if __name__ == '__main__':
-    mqtt_connection = cmdUtils.build_mqtt_connection(on_connection_interrupted, on_connection_resumed)
+    mqtt_connection = cmdUtils.build_mqtt_connection(
+        on_connection_interrupted, on_connection_resumed)
 
     if is_ci == False:
         print("Connecting to {} with client ID '{}'...".format(
@@ -102,20 +115,24 @@ if __name__ == '__main__':
     # This step loops forever if count was set to 0.
     if message_string:
         if message_count == 0:
-            print ("Sending messages until program killed")
+            print("Sending messages until program killed")
         else:
-            print ("Sending {} message(s)".format(message_count))
+            print("Sending {} message(s)".format(message_count))
 
         publish_count = 1
         while (publish_count <= message_count) or (message_count == 0):
-            message = "{} [{}]".format(message_string, publish_count)
-            print("Publishing message to topic '{}': {}".format(message_topic, message))
+            rand_temp = random2.randint(20, 40)
+            rand_humidity = random2.randint(50, 100)
+            message = {
+                "temperature": rand_temp,
+                "humidity": rand_humidity
+            }
             message_json = json.dumps(message)
             mqtt_connection.publish(
                 topic=message_topic,
                 payload=message_json,
                 qos=mqtt.QoS.AT_LEAST_ONCE)
-            time.sleep(1)
+            time.sleep(3)
             publish_count += 1
 
     # Wait for all messages to be received.
